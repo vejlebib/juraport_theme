b06ccb51385dca33a44f610dba68654b9f2a1705
Simon Holt <simonholt83@gmail.com>
2015-02-25 16:45:53 +0100

    Added wrapper div around logo-image to make styling easier.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -15,7 +15,9 @@
   <header class="header" id="header" role="banner">
 
     <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+      <div class="header__logo-wrapper" id="logo-wrapper">
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+      </div>      
     <?php endif; ?>
 
     <?php if ($site_name || $site_slogan): ?>
