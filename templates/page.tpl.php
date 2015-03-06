ed2e62871fc616eb9de862e9c30252d940f6b8ce
Simon Holt <simonholt83@gmail.com>
2015-03-06 15:52:00 +0100

    Removed feed-icons from page template.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -52,7 +52,6 @@
           <ul class="action-links"><?php print render($action_links); ?></ul>
         <?php endif; ?>
         <?php print render($page['content']); ?>
-        <?php print $feed_icons; ?>
       </div>
     </div>
 
